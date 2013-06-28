4133d3ddf793b56439e1b56d5d4859a336df3cc9
Jennifer Eliuk <jen7maples@gmail.com>
2013-06-27 19:01:40 -0600

    Add validation tests for votes

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Vote do
+
+  it "is valid with a user_id and question_id" do
+    vote = Vote.new(user_id: 1, question_id: 1)
+    expect(vote).to be_valid
+  end
+
+  it "should be invalid without a user_id" do
+    vote = Vote.new(track_id: 1)
+    expect(vote).to have(1).errors_on(:user_id)
+  end
+
+  it "should be invalid without a track_id" do
+    vote = Vote.new(user_id: 1)
+    expect(vote).to have(1).errors_on(:track_id)
+  end
+end
